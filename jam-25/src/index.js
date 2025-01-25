420f3be6e58208f7f00b630ad3408430e2eecda4
Graham Pyett <graham@theinteractiveabode.com>
2025-01-24 20:38:43 -0600

    eslint

--- jam-25/src/index.js
+++ jam-25/src/index.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+// eslint-disable-next-line no-unused-vars
 const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
