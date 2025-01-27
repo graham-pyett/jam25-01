198de02ca1c371a6bc8b68b9d47135b86b1c8309
Graham Pyett <graham@theinteractiveabode.com>
2025-01-26 18:13:41 -0600

    cornucopia fix

--- jam-25/src/routes/App.jsx
+++ jam-25/src/routes/App.jsx
@@ -127,7 +127,7 @@ const App = () => {
     const invalidScore = words[currentTurnRef.current].filter((w) => !w.valid).reduce((acc, w) => acc + scoreWord(w), 0);
     let currentScore = validScore - invalidScore;
     jokers.forEach((j) => {
-      const { newScore, newMoney } = j.props?.joker?.action?.({ words: words, grid: gridArray, totalScore: currentScore, validScore, invalidScore }) ?? { newScore: 0, newMoney: 0 };
+      const { newScore, newMoney } = j.props?.joker?.action?.({ words: words, grid: gridArray, totalScore: currentScore, validScore, invalidScore }) ?? { newScore: currentScore, newMoney: 0 };
       currentScore = newScore;
       setFunds((old) => old + newMoney);
     });
